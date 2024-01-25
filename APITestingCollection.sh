0f0456dd69642d310dd0bbd1e1b791eef11d7d1d
yussuf-codes <yussuf.code@gmail.com>
2024-01-25 10:36:53 -0800

    improving

--- APITestingCollection.sh
+++ APITestingCollection.sh
@@ -1,9 +1,9 @@
-curl -X GET -s http://localhost:8000/api/users/ | jq
+curl -X 'GET' -H 'accept: text/plain' -s http://localhost:8000/api/users/ | jq
 
-curl -X GET -s http://localhost:8000/api/users/1/ | jq
+curl -X 'GET' -H 'accept: text/plain' -s http://localhost:8000/api/users/1/ | jq
 
-curl -X POST -H 'Content-Type: application/json' -d @input.json -s http://localhost:8000/api/users/ | jq
+curl -X 'POST' -H 'Content-Type: application/json' -d @input.json -s http://localhost:8000/api/users/ | jq
 
-curl -X PUT -H 'Content-Type: application/json' -d @input.json -s http://localhost:8000/api/users/1
+curl -X 'PUT' -H 'Content-Type: application/json' -d @input.json -s http://localhost:8000/api/users/1
 
-curl -X DELETE -s http://localhost:8000/api/users/1/
+curl -X 'DELETE' -s http://localhost:8000/api/users/1/
