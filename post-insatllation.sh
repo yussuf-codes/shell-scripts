aa326b07d363ff31b6f1da2c2c5a92d280bc5e45
Youssef Saleh <101816502+yussuf-codes@users.noreply.github.com>
2024-04-02 16:45:08 -0700

    Update post-insatllation.sh

--- post-insatllation.sh
+++ post-insatllation.sh
@@ -2,5 +2,6 @@ sudo apt update && sudo apt full-upgrade
 
 sudo apt install git
 
-sudo apt install tree
-sudo apt install jq
+sudo apt install tree -y
+sudo apt install openssh-server -y
+sudo apt install jq -y
