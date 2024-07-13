5242a6c0bd2b782bc9da581b50e8c333aa873360
Youssef Saleh <101816502+yussuf-codes@users.noreply.github.com>
2024-07-13 21:45:05 +0300

    Update dotnetLinux.sh

--- Bash Scripts/dotnetLinux.sh
+++ Bash Scripts/dotnetLinux.sh
@@ -1,3 +1,15 @@
-wget -q -P ~/Downloads <url>
-mkdir ~/bin/dotnet
-tar zxf ~/Downloads/dotnet-sdk-<version>-linux-x64.tar.gz -C ~/bin/dotnet
+if [ ! -d $HOME/downloads ]; then
+  mkdir $HOME/downloads
+fi
+
+if [ ! -d $HOME/bin/ ]; then
+  mkdir $HOME/bin/
+fi
+
+mkdir $HOME/bin/dotnet-sdk-8-linux-x64
+
+wget -q -P $HOME/downloads -O dotnet-sdk-8-linux-x64.tar.gz $1
+
+tar zxf $HOME/downloads/dotnet-sdk-8-linux-x64.tar.gz -C $HOME/bin/dotnet-sdk-8-linux-x64
+
+printf '\nexport DOTNET_ROOT=$HOME/bin/dotnet-sdk-8-linux-x64\nexport PATH=$PATH:$HOME/bin/dotnet-sdk-8-linux-x64:$HOME/.dotnet/tools\n' >> $HOME/.bashrc
