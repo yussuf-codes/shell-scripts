1b10d0d7aceb9a88c579c40df896c6b394f64b57
Youssef Saleh <101816502+yussuf-codes@users.noreply.github.com>
2024-07-17 09:03:36 +0300

    Update mssql-server.sh

--- Bash Scripts/mssql-server.sh
+++ Bash Scripts/mssql-server.sh
@@ -5,8 +5,11 @@ curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trust
 curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | sudo tee /etc/apt/sources.list.d/mssql-server-2022.list
 
 sudo apt update
+
 sudo apt install mssql-server -y
 
 sudo /opt/mssql/bin/mssql-conf setup
 
 systemctl status mssql-server --no-pager
+
+sudo ufw allow 1433/tcp
