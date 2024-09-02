e9364fe225a9fd779a33e206d5c1d15089f1e511
yussuf-codes <yussuf.code@gmail.com>
2024-09-01 23:07:08 -0700

    update

--- Bash Scripts/configureGit.sh
+++ Bash Scripts/configureGit.sh
@@ -1,4 +1,3 @@
+git config --global init.defaultBranch master &&
 git config --global user.name yussuf-codes &&
 git config --global user.email yussuf.code@gmail.com &&
-git config --global init.defaultBranch master &&
-git config --global color.ui auto
--- Bash Scripts/dotnetLinux.sh
+++ Bash Scripts/dotnetLinux.sh
@@ -1,15 +1,15 @@
-if [ ! -d $HOME/downloads ]; then
-  mkdir $HOME/downloads
+if [ ! -d ~/downloads ]; then
+  mkdir ~/downloads
 fi
 
-if [ ! -d $HOME/bin/ ]; then
-  mkdir $HOME/bin/
+if [ ! -d ~/bin/ ]; then
+  mkdir ~/bin/
 fi
 
-mkdir $HOME/bin/dotnet-sdk-8-linux-x64
+mkdir ~/bin/dotnet-sdk-8-linux-x64
 
-curl $1 -o $HOME/downloads/dotnet-sdk-8-linux-x64.tar.gz
+curl $1 -o ~/downloads/dotnet-sdk-8-linux-x64.tar.gz
 
-tar zxf $HOME/downloads/dotnet-sdk-8-linux-x64.tar.gz -C $HOME/bin/dotnet-sdk-8-linux-x64
+tar zxf ~/downloads/dotnet-sdk-8-linux-x64.tar.gz -C ~/bin/dotnet-sdk-8-linux-x64
 
-printf '\nexport DOTNET_ROOT=$HOME/bin/dotnet-sdk-8-linux-x64\nexport PATH=$PATH:$HOME/bin/dotnet-sdk-8-linux-x64:$HOME/.dotnet/tools\n' >> $HOME/.bashrc
+printf '\nif [ -d ~/bin/dotnet-sdk-8-linux-x64 ]; then\n\texport DOTNET_ROOT=~/bin/dotnet-sdk-8-linux-x64\n\texport PATH=$PATH:~/bin/dotnet-sdk-8-linux-x64:~/.dotnet/tools\nfi\n' >> ~/.bashrc
