53db14ddc625877abe6f39122113d4271e17ca6d
yussuf-codes <yussuf.code@gmail.com>
2024-09-04 13:47:49 +0300

    improvements

--- Bash Scripts/mysql-installation.sh
+++ Bash Scripts/mysql-installation.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt install mysql-server -y
 systemctl status mysql.service
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by 'P@ssw0rd';"
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by '$1';"
