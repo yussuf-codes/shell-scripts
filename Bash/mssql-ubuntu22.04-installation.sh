8094bc27e3b1e23980047f862b4ee8b38a5a177d
yussuf-codes <yussuf.code@gmail.com>
2024-09-29 11:37:30 +0300

    updated sql installation scripts

--- Bash/mssql-ubuntu22.04-installation.sh
+++ Bash/mssql-ubuntu22.04-installation.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+password='Passw0rd'
+create_database_query='CREATE DATABASE Playground;'
+
 curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
 curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | sudo tee /etc/apt/sources.list.d/mssql-server-2022.list
+
 sudo apt update
 sudo apt install mssql-server -y
 sudo /opt/mssql/bin/mssql-conf setup
+
 systemctl status mssql-server --no-pager
+
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/22.04/prod.list)"
+sudo apt install sqlcmd -y
+
+sqlcmd -U sa -P ""$password -Q "$create_database_query"
+
 sudo ufw allow 1433/tcp
--- Bash/mysql-ubuntu22.04-installation.sh
+++ Bash/mysql-ubuntu22.04-installation.sh
@@ -3,5 +3,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install mysql-server -y
+
 systemctl start mysql.service
+
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by '$1';"
