dea8beabed508fab3dd5d05dad824f9f400bfbf2
Youssef Saleh <101816502+yussuf-codes@users.noreply.github.com>
2024-10-26 01:15:34 +0300

    Update configure-git.sh

--- Bash/configure-git.sh
+++ Bash/configure-git.sh
@@ -1,3 +1,5 @@
-git config --global init.defaultBranch master &&
-git config --global user.name yussuf-codes &&
+git config --global init.defaultBranch master
+git config --global core.editor nano
+git config --global user.name yussuf-codes
 git config --global user.email yussuf.code@gmail.com
+git config --global color.ui true
