daf97b38b9a1d80d2c987c807fde59e8922d9ed9
Youssef Saleh <yusssuf106@gmail.com>
2024-12-20 14:48:10 +0200

    improvements

--- Bash/dotnet-linux-installation.sh
+++ Bash/dotnet-linux-installation.sh
@@ -1,15 +1,17 @@
-if [ ! -d ~/downloads ]; then
-  mkdir ~/downloads
+#!/bin/bash
+
+if [ ! -d ~/downloads/ ]; then
+	mkdir ~/downloads/
 fi
 
 if [ ! -d ~/bin/ ]; then
-  mkdir ~/bin/
+	mkdir ~/bin/
 fi
 
-mkdir ~/bin/dotnet-sdk-8-linux-x64
+mkdir ~/bin/dotnet-sdk-9-linux-x64
 
-curl $1 -o ~/downloads/dotnet-sdk-8-linux-x64.tar.gz
+curl "$1" -o ~/downloads/dotnet-sdk-9-linux-x64.tar.gz
 
-tar zxf ~/downloads/dotnet-sdk-8-linux-x64.tar.gz -C ~/bin/dotnet-sdk-8-linux-x64
+tar zxf ~/downloads/dotnet-sdk-9-linux-x64.tar.gz -C ~/bin/dotnet-sdk-9-linux-x64
 
-printf '\nif [ -d ~/bin/dotnet-sdk-8-linux-x64 ]; then\n\texport DOTNET_ROOT=~/bin/dotnet-sdk-8-linux-x64\n\texport PATH=$PATH:~/bin/dotnet-sdk-8-linux-x64:~/.dotnet/tools\nfi\n' >> ~/.bashrc
+printf '\nif [ -d ~/bin/dotnet-sdk-9-linux-x64 ]; then\n\texport DOTNET_ROOT=~/bin/dotnet-sdk-9-linux-x64\n\texport PATH=$PATH:~/bin/dotnet-sdk-9-linux-x64:~/.dotnet/tools\nfi\n' >> ~/.bashrc
--- Bash/ubuntu-post-insatllation.sh
+++ Bash/ubuntu-post-insatllation.sh
@@ -5,7 +5,6 @@ sudo apt update && sudo apt full-upgrade
 sudo apt install cmatrix -y
 sudo apt install ffmpeg -y
 sudo apt install gcc -y
-sudo apt install git -y
 sudo apt install jq -y
 sudo apt install neofetch -y
 sudo apt install tree -y
