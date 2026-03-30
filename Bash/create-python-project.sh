3c2add34c6fa922b7a06d25f0e9920257d905053
Youssef Saleh <yusssuf106@gmail.com>
2026-03-30 23:35:01 +0200

    Enhancements

--- Bash/create-python-project.sh
+++ Bash/create-python-project.sh
@@ -2,11 +2,12 @@
 
 mkdir ./$1/ &&
 cd ./$1/ &&
-touch ./main.py &&
 python3 -m venv ./.venv/ &&
+touch ./README.md &&
+mkdir ./src &&
+touch ./src/main.py &&
 curl https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore -o ./.gitignore -s &&
 git init &&
 git add . &&
-git commit -m "Initializing project" &&
+git commit -m "Initial commit" &&
 source ./.venv/bin/activate &&
-code .
